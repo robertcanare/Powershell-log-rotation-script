34724146e2c02922a942cbffd8be1bfcb5ba5c8b
Robert John P. Canare <33864725+robertcanare@users.noreply.github.com>
2021-01-19 09:25:25 +0100

    Add files via upload

---
+++ init-1-first-execution.ps1
@@ -0,0 +1,41 @@
+﻿# Windows Server Log rotation script
+
+# Get the current date 
+$current_date = $(Get-Date -Format "MM-dd-yyyy-HHmm")
+$current_year = $(Get-Date -Format "yyyy")
+
+# Location to the root directory for the log files
+$root_location = "C:\Users\rcanare\Desktop\test-logs-directory\"
+
+# File extention
+$file_extention = "*.log"
+
+# By days
+$compress_duration = "-6"
+$delete_archived = "-365"
+
+# Scraping each directory
+$directory = $(Get-ChildItem "$root_location" -Recurse -Directory)
+foreach ($d in $directory) {
+    $location = $d.FullName + "\"
+    for ($i = 1; $i -le 13; $i++) { 
+    $month = $((Get-Culture).DateTimeFormat.GetMonthName($i)) 
+    $two_digits = $("{0:D2}" -f $i)
+    $creation_time = "$two_digits/30/2020 12:42 AM"
+    $file = "$location$month-$current_year.zip"
+
+
+    # Compression for the PS version 4.0
+    $files = $(Get-ChildItem "$location" -Recurse -File -Filter $file_extention | ?{$_.CreationTime.Month -eq $i}) 
+
+    foreach ($f in $files) {
+    $outfile = $f.FullName 
+    md $location$month-$current_year
+
+    mv $outfile $location$month-$current_year
+    .\Rar.exe a $location$month-$current_year.zip $location$month-$current_year ; Remove-Item -Path $location$month-$current_year -Recurse -Force ; (Get-ChildItem $location$month-$current_year.zip ).CreationTime = $creation_time 
+                         }
+
+
+                                } 
+                        }
---
+++ init-2-taskscheduler.ps1
@@ -0,0 +1,31 @@
+﻿# Windows Server Log rotation script
+
+# Get the current date 
+$current_date = $(Get-Date -Format "MM-dd-yyyy-HHmm")
+$current_year = $(Get-Date -Format "yyyy")
+
+# Location to the root directory for the log files
+$root_location = "C:\Users\rcanare\Desktop\test-logs-directory\"
+
+# File extention
+$file_extention = "*.log"
+
+# By days
+$compress_duration = "-6"
+$delete_archived = "-365"
+
+# Scrape each directory
+$directory = $(Get-ChildItem "$root_location" -Recurse -Directory)
+foreach ($d in $directory) {
+    $location = $d.FullName + "\"
+    $files = $(Get-ChildItem "$location" -Recurse -File -Filter $file_extention | ? {$_.CreationTime -ge (Get-Date).AddDays(-6)})
+    foreach ($f in $files) {
+    $outfile = $f.FullName
+    md $location$current_date-$current_year
+    mv $outfile $location$current_date-$current_year
+    .\Rar.exe a $location$current_date-$current_year.zip $location$current_date-$current_year ; Remove-Item -Path $location$current_date-$current_year -Recurse -Force
+
+    # Deleting old compressed files older than $delete_archived
+    Get-ChildItem "$location" -Recurse -File -Filter "*.zip" | Where CreationTime -lt  (Get-Date).AddDays($delete_archived) | Remove-Item -Force
+                        }
+                            }
